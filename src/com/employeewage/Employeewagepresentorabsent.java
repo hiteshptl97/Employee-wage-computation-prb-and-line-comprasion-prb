ee1c11c2d13f4e444b2af1ec78066de3d9bbcdbc
hiteshptl97 <hiteshpatil897@gmail.com>
2022-10-11 21:10:11 +0530

    employee wage computation and line comparision problem done

---
+++ src/com/employeewage/Employeewagepresentorabsent.java
@@ -0,0 +1,25 @@
+package com.employeewage;
+
+//This program is to check whether the Employee is present or not.
+
+public class Employeewagepresentorabsent {
+
+        public static void main(String[] args) {
+
+            //Declaring the variables.
+            double isPresent = 1;
+            double randomCheck = (double)Math.floor((Math.random()*10)%2);
+
+            //Checking if the Employee is present!!!
+            if (randomCheck == isPresent)
+            {
+                System.out.println("Employee is present");
+            }
+            else
+            {
+                System.out.println("Employee is absent");
+            }
+        }
+    }
+
+
